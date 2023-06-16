a2c71aacb324eab55a711e1425c233854567d003
maximo147 <max1901@hotmail.com>
2023-06-16 02:29:03 -0500

    first commit

---
+++ src/test/java/com/example/backendinventory/BackendInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
